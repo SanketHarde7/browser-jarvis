3a4d29b2a5fcbff3d5cf684b68818c87fdc1918a
sanket <sanketharde62@gmail.com>
2026-07-29 18:42:02 +0530

    fix(mobile): update MainActivity for background service registration

--- max-mobile/android/app/src/main/java/com/jarvis/maxmobile/MainActivity.java
+++ max-mobile/android/app/src/main/java/com/jarvis/maxmobile/MainActivity.java
@@ -1,5 +1,21 @@
 package com.jarvis.maxmobile;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Auto-start background service
+        Intent serviceIntent = new Intent(this, MaxBackgroundService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+}
